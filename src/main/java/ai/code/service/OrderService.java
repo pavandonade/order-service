b7a9677980d8794ea20e7811100a25d4d9ff0db0
Pavan Donade <pavandonade.dev@gmail.com>
2026-04-09 19:23:47 +0530

    kafka config

--- src/main/java/ai/code/service/OrderService.java
+++ src/main/java/ai/code/service/OrderService.java
@@ -34,7 +34,11 @@ public class OrderService {
             orderRepository.save(order);
 
             // Send the message to Kafka topic
-            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
             log.info("Start - Sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
             kafkaTemplate.send("Order-placed", orderPlacedEvent);
             log.info("End - Sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
